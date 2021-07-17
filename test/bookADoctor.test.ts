0c04bbecf40dfb0bd349873dac5a15188d01c912
vuongpsht <Hoang-thien-vuong.phu-sanh@ant-tech.eu>
2021-07-17 14:54:32 +0700

    change tobe to toStrictEqual

--- test/bookADoctor.test.ts
+++ test/bookADoctor.test.ts
@@ -2,39 +2,41 @@ import {bookDoctorStore} from '../src/store/BookDoctorStore';
 import {constantData} from '../src/constant/ConstantData';
 
 it('init selected doctors should be zero', function () {
-  expect(bookDoctorStore.selectedDoctors.length).toBe(0);
+  expect(bookDoctorStore.selectedDoctors.length).toStrictEqual(0);
 });
 
 it('init selected symptom should be zero', function () {
-  expect(bookDoctorStore.selectedSymptom.length).toBe(0);
+  expect(bookDoctorStore.selectedSymptom.length).toStrictEqual(0);
 });
 
 it('init favorite should be 11', function () {
-  expect(bookDoctorStore.favoriteSymptom.length).toBe(11);
+  expect(bookDoctorStore.favoriteSymptom.length).toStrictEqual(11);
 });
 
 it('init personal symptom should be 8', function () {
-  expect(bookDoctorStore.personalSymptom.length).toBe(8);
+  expect(bookDoctorStore.personalSymptom.length).toStrictEqual(8);
 });
 
 it('selected doctor should add to selectedDoctors, add element name Ana', function () {
   bookDoctorStore.dispatchSelectedList('Ana');
-  expect(bookDoctorStore.selectedDoctors[0]).toBe('Ana');
+  expect(bookDoctorStore.selectedDoctors[0]).toStrictEqual('Ana');
 });
 
 it('when selected doctor dispatch again should remove Ana from selectedDoctors', function () {
   bookDoctorStore.dispatchSelectedList('Ana');
-  expect(bookDoctorStore.selectedDoctors.length).toBe(0);
+  expect(bookDoctorStore.selectedDoctors.length).toStrictEqual(0);
 });
 
 it('selected symptom with first element of constant.symptomList[4]', function () {
   bookDoctorStore.dispatchToggleSelectedList(constantData.symptomList[4]);
-  expect(bookDoctorStore.selectedSymptom[0]).toBe(constantData.symptomList[4]);
+  expect(bookDoctorStore.selectedSymptom[0]).toStrictEqual(
+    constantData.symptomList[4],
+  );
 });
 
 it('selected symptom length should be zero when re-dispatch', function () {
   bookDoctorStore.dispatchToggleSelectedList(constantData.symptomList[4]);
-  expect(bookDoctorStore.selectedSymptom.length).toBe(0);
+  expect(bookDoctorStore.selectedSymptom.length).toStrictEqual(0);
 });
 
 it("time should be {date: '17 July', hours: '12:02' }", function () {
